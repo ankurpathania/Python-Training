857cfbaf0cbc6ca44113fc9362418af040202c1e
Ankur <ankur.pathania2990@gmail.com>
2021-09-05 17:36:47 -0700

    Ticket-43214- Pyspark Mean Function added

--- Spark/Function-test3.py
+++ Spark/Function-test3.py
@@ -1,1 +1,7 @@
-print("Hello PySpark")
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
+
+df_pyspark = spark.read.option('header','true').csv('test.csv')
+df_pyspark.show()
+
